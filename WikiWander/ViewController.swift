b668d1ef8e9fd118532ceb4ab15a1fdd88ceb8ad
Bethany Drake <bethanyd@thoughtworks.com>
2018-12-13 14:25:34 +1100

    basic https request!

--- WikiWander/ViewController.swift
+++ WikiWander/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionTaskDelegate {
 
     @IBOutlet weak var articleTextBox: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+//    func urlSession(_ session: URLSession, task: URLSessionTask, didRecieve: Data) {
+//        print("recieved" + String(data: didRecieve, encoding: .utf8)!)
+//
+//    }
 
     @IBAction func nextButton(_ sender: UIButton) {
-        articleTextBox.text = "next"
+        articleTextBox.text = "loading..."
+        
+        
+        let url = URL(string: "https://en.wikipedia.org/w/api.php")!
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            guard let data = data else { return }
+            
+            let newText = String(data: data, encoding: .utf8)!
+            DispatchQueue.main.async {
+                self.articleTextBox.text = newText
+            }
+            print(newText)
+            print("finished")
+        }
+        
+        task.resume()
+        
+        
+    
     }
     
 }
