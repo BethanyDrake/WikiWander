ce78067a44d88a7e15713a30a46d43ee970d6645
Bethany Drake <bethanyd@thoughtworks.com>
2018-12-13 12:08:14 +1100

    Initial Commit

---
+++ WikiWander/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiWander
+//
+//  Created by Bethany Anne Drake on 13/12/18.
+//  Copyright © 2018 WordWatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
