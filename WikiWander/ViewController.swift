da0a7422a122a929c4eb6995997fd6a5a33eb740
Bethany Drake <bethanyd@thoughtworks.com>
2018-12-14 11:10:05 +1100

    display only <p></p> content

--- WikiWander/ViewController.swift
+++ WikiWander/ViewController.swift
@@ -16,25 +16,63 @@ class ViewController: UIViewController, URLSessionTaskDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-//    func urlSession(_ session: URLSession, task: URLSessionTask, didRecieve: Data) {
-//        print("recieved" + String(data: didRecieve, encoding: .utf8)!)
-//
-//    }
+    
+    
+    func getArticalContent(text: String)->String {
+        //first off, lets just get the <p>s. Then we can add headings.
+        //let index = text.index(after: "<p>")
+        //var index = 0;
+        var open = false
+        var plainText = ""
+        
+        var toRead = text
+        
+        var tags = [""]
+        
+        while (toRead.count > 0) {
+            if !open && toRead.prefix(3) == "<p>"{
+                print (text)
+                toRead = String(toRead.dropFirst(3))
+                open = true
+                continue
+            }
+            
+            if open && toRead.prefix(3) == "</p>"{
+                toRead = String(toRead.dropFirst(4))
+                plainText += "\n\n"
+                open = false
+                continue
+            }
+            
+            if open {
+                plainText += toRead.prefix(1)
+            }
+            
+            toRead = String(toRead.dropFirst(1))
+        }
+        
+        return plainText
+    }
 
     @IBAction func nextButton(_ sender: UIButton) {
         articleTextBox.text = "loading..."
         
         
-        let url = URL(string: "https://en.wikipedia.org/w/api.php")!
+        let url = URL(string: "https://en.wikipedia.org/wiki/Cookie")!
         let task = URLSession.shared.dataTask(with: url) {
             (data, response, error) in
             guard let data = data else { return }
             
-            let newText = String(data: data, encoding: .utf8)!
+            let text = String(data: data, encoding: .utf8)!
+            
+            let articalContent = self.getArticalContent(text: text)
+            
+            
+            
             DispatchQueue.main.async {
-                self.articleTextBox.text = newText
+                self.articleTextBox.text = articalContent
             }
-            print(newText)
+            print(articalContent)
             print("finished")
         }
         
